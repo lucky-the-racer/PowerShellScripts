95cbde9a0c4eac822e01a161ce29aeeedebf6a30
lucky-the-racer <114669843+lucky-the-racer@users.noreply.github.com>
2024-05-24 17:37:15 +0530

    Delete SetPermissions.ps1

--- SetPermissions.ps1
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Creating example.txt
-echo This is a test file. > example.txt
-echo Setting permissions...
-icacls example.txt /grant %USERNAME%:F
-icacls example.txt /grant "Users":RX
-icacls example.txt /grant "Everyone":R
-echo Permissions set successfully.
-pause
